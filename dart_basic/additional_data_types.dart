cc2c69b68f5132fa3667e230e1b9cbc2c69e6848
Rifyalaidil <rifyalaidil05@gmail.com>
2025-03-03 21:13:22 +0700

    dart variables and data types 1

---
+++ dart_basic/additional_data_types.dart
@@ -0,0 +1,16 @@
+void main() {
+// List
+  List<String> nameName = ['Yumizuno', 'Marytheblue', 'Shampoo'];
+  List<int> numberNumber = [1, 2, 3];
+  List<dynamic> mixed = [1, "Two", true];
+  
+  print("Output");
+  print("name : ${nameName[0]}");
+  print("number : $numberNumber");
+  print("Mixed : $mixed");
+  print("Mixed-integer : ${mixed[0]}");
+  print("Mixed-string : ${mixed[1]}");
+  print("Mixed-bool : ${mixed[2]}");
+
+// Map
+}
